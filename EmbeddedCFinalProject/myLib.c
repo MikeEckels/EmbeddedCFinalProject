8e15244b8a8067c92406e5b10ccf1472af4fb253
MikeEckels <32800922+MikeEckels@users.noreply.github.com>
2021-04-15 22:21:14 -0400

    Optimized and added dist/slope functions

--- EmbeddedCFinalProject/myLib.c
+++ EmbeddedCFinalProject/myLib.c
@@ -1,4 +1,5 @@
 #include "myLib.h"
+#include <Math.h>
 
 float cToF(float degC) {
 	return (float)(degC * (9.0F / 5.0F) + 32.0F);
@@ -10,90 +11,57 @@ float fToC(float degF) {
 
 Cylinder nextCyl(Cylinder nowCyl) {
 	Cylinder newCyl;
+	newCyl = (Cylinder)(nowCyl == NUM_CYLS) || (nowCyl < 0) ? CYL_ERROR : nowCyl + 1;
+	newCyl = (Cylinder)(((nowCyl < NUM_CYLS) && (nowCyl > 0)) && (newCyl >= NUM_CYLS)) ? 0: newCyl;
 
-	switch (nowCyl) {
-		case L1:
-			newCyl = R1;
-			break;
-		case L2:
-			newCyl = R2;
-			break;
-		case R1:
-			newCyl = L2;
-			break;
-		case R2:
-			newCyl = L1;
-			break;
-		default:
-			newCyl = CYL_ERROR;
-	}
 	return newCyl;
 }
 
 DayOfWeek dayOfWeek(Date date) {
 	const int subtractionFactor = 621049;
-	Date dateZero = { 0 };
-	DayOfWeek dow;
-
-	int days = daysBetween(dateZero, date);
+	int days = numDays(date);
 	days -= subtractionFactor;
-	days %= 7;
 
-	switch (days) {
-	case 0:
-		dow = SUNDAY;
-		break;
-	case 1:
-		dow = MONDAY;
-		break;
-	case 2:
-		dow = TUESDAY;
-		break;
-	case 3:
-		dow = WEDNESDAY;
-		break;
-	case 4:
-		dow = THURSDAY;
-		break;
-	case 5:
-		dow = FRIDAY;
-		break;
-	case 6:
-		dow = SATURDAY;
-		break;
-	default:
-		dow = -1;
-	}
+	return ((DayOfWeek)days % 7);
+}
 
-	return dow;
+int numDays(Date d1) {
+	d1.year = (d1.month <= 2) ? d1.year - 1 : d1.year;
+	d1.month = (d1.month <= 2) ? d1.month + 13 : d1.month + 1;
+	int numDays = (((1461 * d1.year) / (4)) + ((153 * d1.month) / (5)) + d1.day);
+
+	return numDays;
 }
 
 int daysBetween(Date d1, Date d2) {
-	
-	d1.year = (d1.month <= 2) ? d1.year - 1 : d1.year;
-	d2.year = (d2.month <= 2) ? d2.year - 1 : d2.year;
-
-	d1.month = (d1.month <= 2) ? d1.month + 13 : d1.month + 1;
-	d2.month = (d2.month <= 2) ? d2.month + 13 : d2.month + 1;
-
-	int numDays1 = (((1461 * d1.year)/(4)) + ((153 * d1.month)/(5)) + d1.day);
-	int numDays2 = (((1461 * d2.year) / (4)) + ((153 * d2.month) / (5)) + d2.day);
+	int numDays1 = numDays(d1);
+	int numDays2 = numDays(d2);
 
 	return (numDays2 - numDays1);
 }
 
 float distance(const Point p1, const Point p2) {
+	float power1 = powf(p2.x - p1.x, 2);
+	float power2 = powf(p2.y - p1.y, 2);
 
+	return (float)(fabs((sqrtf(power1 + power2))));
 }
 
 SlopeIntercept slopeIntercept(const Point p1, const Point p2) {
+	SlopeIntercept si;
+	si.slope = ((p2.y - p1.y) / (p2.x - p1.x));
+	si.yIntercept = (-(si.slope * p1.x) + p1.y);
+	si.valid = (p1.x - p2.x) == 0 ? false:true ;
 
+	return si;
 }
 
 LinearInterp linearInterp(const Point p1, const Point p2, const float x, const bool limit) {
-
+	LinearInterp lerp = { 0 };
+	return lerp;
 }
 
 Ieee754singleFactors  computeIeee754singleFactors(const Ieee754singlePrecision ieeeNum) {
-
+	Ieee754singleFactors ieee = { 0 };
+	return ieee;
 }
